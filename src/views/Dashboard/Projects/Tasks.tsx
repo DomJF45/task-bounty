5c2fbb98c9515f7e7d1a4b02ff49ff2109b80a9c
balsac7 <70721744+balsac7@users.noreply.github.com>
2023-07-10 14:23:21 -0400

    added tasks

--- src/views/Dashboard/Projects/Tasks.tsx
+++ src/views/Dashboard/Projects/Tasks.tsx
@@ -1,17 +1,58 @@
 import { useState } from "react";
-import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Heading,
+  SimpleGrid,
+  Text,
+  Icon,
+} from "@chakra-ui/react";
 import { dataBase, iTask } from "../../../data/dataBase";
 import { iProject } from "../../../data/dataBase";
 import { useCallback, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from "../../../components/Card";
+import { FaCoins } from "react-icons/fa6";
+
+interface TaskProps {
+  task: iTask;
+}
+
+const Task: React.FC<TaskProps> = ({ task }) => {
+  return (
+    <Card width={300} height={200}>
+      <HStack justifyContent={"space-between"}>
+        <CardHeader>{task.name}</CardHeader>
+        <CardHeader size={"xs"} textDecoration={"none"}>
+          <Icon as={FaCoins} color={"orange.500"} mr={1} />
+          <Text as={"span"}>{task.yeild}</Text>
+        </CardHeader>
+      </HStack>
+      <CardBody>
+        <Heading>Heading</Heading>
+      </CardBody>
+      <CardFooter unitId={task.id}>
+        <Button>Complete</Button>
+      </CardFooter>
+    </Card>
+  );
+};
 
 const Tasks = () => {
   const [projectData, setProjectData] = useState<iProject>();
   const { projectName } = useParams();
   console.log(projectName);
+
   function getProjectFromDB(name: string): iProject {
     const { projects } = dataBase;
-    return projects.filter((project: iProject) => project.name != name)[0];
+    return projects.filter((project: iProject) => project.name === name)[0];
   }
 
   const setProject = useCallback(() => {
@@ -22,22 +63,23 @@ const Tasks = () => {
     }
   }, [projectName]);
 
+  console.log(projectData?.name);
+
   useEffect(() => {
     setProject();
   }, [setProject]);
 
   return (
-    <Box>
-      <Heading>Tasks</Heading>
-      <Box>
-        <SimpleGrid>
+    <Box width={"100%"} display={"flex"} flexDirection={"column"} rowGap={14}>
+      <Heading>{projectName}</Heading>
+      <Flex gap={10}>
+        <Text size={"lg"}>Current Tasks:</Text>
+        <SimpleGrid flex={1} columns={{ base: 1, md: 2, lg: 3 }}>
           {projectData?.tasks?.map((task: iTask) => (
-            <Box>
-              <Heading>{task.name}</Heading>
-            </Box>
+            <Task task={task} key={task.id} />
           ))}
         </SimpleGrid>
-      </Box>
+      </Flex>
     </Box>
   );
 };
--- src/views/Dashboard/Projects/index.tsx
+++ src/views/Dashboard/Projects/index.tsx
@@ -1,49 +1,21 @@
-import {
-  Box,
-  Flex,
-  Heading,
-  SimpleGrid,
-  Text,
-  Button,
-  Icon,
-  HStack,
-} from "@chakra-ui/react";
+import { Heading, SimpleGrid, Text, Button, Icon } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 import { dataBase } from "../../../data/dataBase";
 import { FiChevronRight } from "react-icons/fi";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from "../../../components/Card";
 
 const Projects = () => {
   return (
     <SimpleGrid columns={{ base: 1, sm: 2, md: 2, lg: 3 }} flex={1} rowGap={14}>
       {dataBase.projects.map((project) => (
-        <Flex
-          direction={"column"}
-          width={"300px"}
-          height={"275px"}
-          border={"1px solid"}
-          borderRadius={"12px"}
-          borderColor={"gray.300"}
-          p={4}
-          position={"relative"}
-        >
-          <Heading
-            size={"md"}
-            textDecoration={"underline"}
-            textDecorationThickness={"1px"}
-            textDecorationColor={"gray.300"}
-            textUnderlineOffset={8}
-          >
-            {project.name}
-          </Heading>
-          <Box
-            height={"100%"}
-            width={"100%"}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            flexDirection={"column"}
-            gap={2}
-          >
+        <Card>
+          <CardHeader>{project.name}</CardHeader>
+          <CardBody>
             <Heading size={"3xl"} position={"relative"}>
               {project.completedTasks < 1 ? (
                 <Text as={"span"} color={"gray.400"}>
@@ -59,16 +31,8 @@ const Projects = () => {
             </Heading>
 
             <Text fontSize={"xs"}>completed</Text>
-          </Box>
-          <HStack
-            position={"absolute"}
-            bottom={3}
-            left={0}
-            width={"100%"}
-            justifyContent={"space-between"}
-            px={3}
-          >
-            <Text alignSelf={"flex-end"}>ID: {project.id}</Text>
+          </CardBody>
+          <CardFooter unitId={project.id}>
             <Button
               variant={"outline"}
               colorScheme="orange"
@@ -78,8 +42,8 @@ const Projects = () => {
             >
               Go To Project <Icon as={FiChevronRight} />
             </Button>
-          </HStack>
-        </Flex>
+          </CardFooter>
+        </Card>
       ))}
     </SimpleGrid>
   );
